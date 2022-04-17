6008c62b4d1cb21c7092febe1be662c0b374769c
Dan <you@example.com>
2022-04-16 21:06:34 -0600

    I created a site for Spanish Fork Utah. It is with cards and a background image.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 2000,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //enable autoplay
+    autoplay: {
+        delay: 5000,
+    }
+  
+  });
+
+
+  const hb = document.querySelector('#hamburgerBtn');
+
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () => {
+     hb.classList.toggle('open')
+     pn.classList.toggle('open')
+
+})
